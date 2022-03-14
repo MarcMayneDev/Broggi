b9187dfc29aa43c766b1b45ad82daa1981a7bc9d
MarcMayneDev <marcmartinez2001@gmail.com>
2022-03-14 13:38:32 +0100

    update marc

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,25 +9,27 @@
 @endsection
 
 @section('main')
-    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+<div class="container w-50" style="height: 200px;">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="img/broggimain.png" class="d-block w-90" alt="...">
+            <img src="img/ambulancia.jpg" class="d-block">
             </div>
             <div class="carousel-item">
-                <img src="img/ambulancia.jpg" class="d-block w-80" alt="...">
+            <img src="img/agente.jpg" height="" class="d-block">
             </div>
             <div class="carousel-item">
-                <img src="img/ambulancia.jpg" class="d-block w-80" alt="...">
+            <img src="img/ambulancia.jpg" class="d-block ">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
+</div>    
 @endsection
