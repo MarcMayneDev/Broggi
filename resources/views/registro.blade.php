f6335c1d8eb7af1b6c28953f8fa894f8f0fccc3e
johnnydaw2a <72017507+johnnydaw2a@users.noreply.github.com>
2022-03-15 22:08:44 +0100

    registro

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('title')
-    Login
+    Registro
 @endsection
 
 @section('main')
@@ -13,8 +13,12 @@
         <form action="#" method="POST">
             <div class="row justify-content-center">
                 <div class="card text-center text-white bg-dark" style="width: 27rem;">
-                    <h2 class="card-title mt-4">Iniciar sesión</h2>
+                    <h2 class="card-title mt-4">Registrarte</h2>
                     <div class="card-body">
+                        <div class="input-group mt-2 mb-3">
+                            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Nombre">
+                            <span class="input-group-text"><img src="img/iconos/user_dark.png" width="15px"></span>
+                        </div>
                         <div class="input-group mt-2 mb-3">
                             <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Correo">
                             <span class="input-group-text"><img src="img/iconos/mail_dark.png" width="15px"></span>
@@ -25,10 +29,10 @@
                         </div>
                         <div class="registro mb-4 d-flex bd-highlight">
                             <div class="p-2 flex-fill bd-highlight align-self-center">
-                                ¿No tienes cuenta? <a href="#" class="text-warning change_link">Regístrate</a>
+                                ¿Tienes cuenta? <a href="#" class="text-warning change_link">Inicia sesión</a>
                             </div>
                             <div class="p-2 flex-fill bd-highlight d-grid col-4">
-                                <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                                <button type="submit" class="btn btn-primary">Crear cuenta</button>
                             </div>
                         </div>
                     </div>
