b47a0eed70e14db56eedf42af574e39ef57e2a28
Marc <marcmartinez2001@gmail.com>
2022-03-29 22:25:34 +0200

    CRUD User basis

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $user = new User();
+        $user->codi = $request->input(‘codi’);
+        $user->contrassenya = $request->input(‘contrassenya’);
+        $user->nom = $request->input(‘nom’);
+        $user->cognoms = $request->input(‘cognoms’);
+
+        try
+        {
+            $user->save();
+        } catch(QueryException $ex){
+            // ToDo -> Crear controlador de mensajes
+            // $message = ControladorMensajes::errorMessage($ex);
+            // $request->session()->flash(‘error’, $message);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->codi = $request->input(‘codi’);
+        $user->contrassenya = $request->input(‘contrassenya’);
+        $user->nom = $request->input(‘nom’);
+        $user->cognoms = $request->input(‘cognoms’);
+
+        try
+        {
+            $user->save();
+        } catch(QueryException $ex){
+            // ToDo -> Crear controlador de mensajes
+            // $message = ControladorMensajes::errorMessage($ex);
+            // $request->session()->flash(‘error’, $message);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        try
+        {
+            $user->delete();
+        } catch(QueryException $ex){
+            // ToDo -> Crear controlador de mensajes
+            // $message = ControladorMensajes::errorMessage($ex);
+            // $request->session()->flash(‘error’, $message);
+        }
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $table = 'usuaris';
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // Id es la clave primaria default en Eloquent e incrementing es true por defecto.
+    // protected $primaryKey = 'id';
+    // public $incrementing = true;
+    public $timestamps = false; // updated_at && created_at
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
